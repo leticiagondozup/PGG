68279b43101b4de591fa8e3a0ac4e9b1bdfbffdc
LeticiaGondo <legondo@outlook.com>
2019-11-04 14:34:15 -0300

    Conexão com a pagina web

---
+++ desafio2/src/main/java/com/exercicios/SegundoDesafio.java
@@ -0,0 +1,25 @@
+package com.exercicios;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class SegundoDesafio {
+
+    public static void main(String[] agrs) throws IOException, InterruptedException {
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest req = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.reddit.com/r/programming/"))
+                .build();
+
+        //Comunicação síncrona
+        HttpResponse<String> response = client.send(req,
+                HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+
+    }
+}
